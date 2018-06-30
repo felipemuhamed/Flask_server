90fa7b9b9db0bc3f228209df6af5d1027645697e
Felipe Muhamed <muhamed@cpqd.com.br>
2018-06-30 15:49:56 -0300

    Added Email/Password reset and WSGI Server

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,29 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from gevent.pywsgi import WSGIServer
+from gevent import monkey
 
+monkey.patch_all()
 app = Flask(__name__)
+WSGI = WSGIServer(('127.0.0.1', 5000), app.wsgi_app)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 
 from app import routes
--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-from app import app
+from app import app, WSGI
+import signal
+
+# use gevent WSGI server instead of the Flask
+http = WSGI
+
+def stop_server(*args, **kwargs):
+    print("Server stopped!")
+    http.stop()
+
+def run_server():
+    print("Running server at: {}:{}".format(http.address[0], http.address[1]))
+    http.serve_forever()
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    signal.signal(signal.SIGINT, stop_server)
+    run_server()
